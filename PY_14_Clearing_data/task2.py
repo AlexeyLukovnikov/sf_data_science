b1dd01cd907f4f7819d2a4b9b997458794018356
Alexey Lukovnikov <alexey.lukovnikov@gmail.com>
2023-01-11 23:52:01 +0300

    Reorgonize structure

---
+++ PY_14_Clearing_data/task2.py
@@ -0,0 +1,55 @@
+# PYTHON-14. Очистка данных
+# 2. Знакомство с новыми данными: данные о квартирах от Сбера
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+pd.set_option('display.expand_frame_repr', False)
+
+sber_data = pd.read_csv('data/sber_data.csv')
+
+# 2.1
+print(sber_data.shape)
+print(sber_data.info())
+# print(sber_data.tail())
+# print(sber_data.isnull().tail())
+
+# 2.2
+# sber_data = sber_data.groupby(by='sub_area').count()
+# print(sber_data.info())
+# print(sber_data.describe())
+# print(sber_data.head())
+
+# 2.3
+# print(sber_data['price_doc'].max())
+# print(sber_data['price_doc'].nlargest(5))
+
+# 2.4
+
+# boxplot = sns.boxplot(
+#     data = sber_data,
+#     x='price_doc',
+#     #orient='h',
+#     y='ecology'
+# )
+
+# 2.5
+# scatterplot = sns.scatterplot(
+#     data = sber_data,
+#     y='price_doc',
+#     x='kremlin_km',
+#     # hue='species',
+#     s=100,
+#     # size='sex',
+#     sizes=(50, 300)
+#
+# )
+# scatterplot.set_title('Зависимость цены от расстояния до кремял ', fontsize=16)
+# scatterplot.set_xlabel('Расстояние до кремля, км')
+# scatterplot.set_ylabel('Цена')
+# #
+# # Постройте диаграмму рассеяния, которая покажет, как цена на квартиру (price_doc) связана с расстоянием до центра Москвы (kremlin_km). В
+#
+# plt.show()
