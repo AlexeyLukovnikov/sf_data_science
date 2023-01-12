3f7d45a7f5f4995a5049c92219b75bb84bb64211
Alexey Lukovnikov <alexey.lukovnikov@gmail.com>
2023-01-12 13:44:52 +0300

    Learning

--- PY_14_Clearing_data/task2.py
+++ PY_14_Clearing_data/task2.py
@@ -10,9 +10,19 @@ pd.set_option('display.expand_frame_repr', False)
 
 sber_data = pd.read_csv('data/sber_data.csv')
 
+
+# print(sber_data.isnull().tail())
 # 2.1
-print(sber_data.shape)
-print(sber_data.info())
+
+cols_null_percent = sber_data.isnull().mean() * 100
+cols_with_null = cols_null_percent[cols_null_percent > 0].sort_values(ascending=False)
+# print(cols_with_null)
+cols_with_null.plot(
+    kind='bar',
+    figsize=(10,4),
+    title="Empty data"
+);
+# print(sber_data.info())
 # print(sber_data.tail())
 # print(sber_data.isnull().tail())
 
